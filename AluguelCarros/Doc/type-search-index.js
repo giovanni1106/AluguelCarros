def3478d17382cbb01aed4ee2bd86ca1de7595da
giovanni1106 <giovanni.acg@gmail.com>
2022-04-07 22:43:07 -0300

    Atualização do java doc

--- AluguelCarros/Doc/type-search-index.js
+++ AluguelCarros/Doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"Pessoas","l":"Admin"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Pessoas","l":"Usuario"}];updateSearchResults();
